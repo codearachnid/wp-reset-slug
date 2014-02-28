46ca741b1931a4090621529594134b3f152c558c
Timothy Wood <tim@imaginesimplicity.com>
2014-02-27 19:55:08 -0500

    updated to match svn repo

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
  
 # main config
-PLUGINSLUG="reset-slug"
+PLUGINSLUG="reset-slugs"
 CURRENTDIR=`pwd`
 MAINFILE="wp-reset-slug.php" # this should be the name of your main php file in the wordpress plugin
  
